fbca6b679fdbac24857b4437436b943ea5874180
vuicoin-project <97007299+vuicoin-project@users.noreply.github.com>
2022-01-05 17:40:15 +0700

    Change LITCOIN to VUICOIN

--- doc/tor.md
+++ doc/tor.md
@@ -1,4 +1,4 @@
-# TOR SUPPORT IN LITECOIN
+# TOR SUPPORT IN VUICOIN
 
 It is possible to run Vuicoin Core as a Tor hidden service, and connect to such services.
 
--- test/functional/test_framework/test_framework.py
+++ test/functional/test_framework/test_framework.py
@@ -141,8 +141,8 @@ class BitcoinTestFramework(metaclass=BitcoinTestMetaClass):
         config = configparser.ConfigParser()
         config.read_file(open(self.options.configfile))
         self.config = config
-        self.options.bitcoind = os.getenv("LITECOIND", default=config["environment"]["BUILDDIR"] + '/src/litecoind' + config["environment"]["EXEEXT"])
-        self.options.bitcoincli = os.getenv("LITECOINCLI", default=config["environment"]["BUILDDIR"] + '/src/litecoin-cli' + config["environment"]["EXEEXT"])
+        self.options.bitcoind = os.getenv("VUICOIND", default=config["environment"]["BUILDDIR"] + '/src/litecoind' + config["environment"]["EXEEXT"])
+        self.options.bitcoincli = os.getenv("VUICOINCLI", default=config["environment"]["BUILDDIR"] + '/src/litecoin-cli' + config["environment"]["EXEEXT"])
 
         os.environ['PATH'] = os.pathsep.join([
             os.path.join(config['environment']['BUILDDIR'], 'src'),
